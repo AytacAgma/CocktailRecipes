29101ce75e2956cf69aeee8e0a9da95fcb442f3b
bkraytac <aytac.agma@sokmarket.com.tr>
2022-12-09 14:02:58 +0300

    GsonBuilder setLenient() was implemented for JSON format error

--- app/src/main/java/com/example/cocktails/service/CocktailAPIService.kt
+++ app/src/main/java/com/example/cocktails/service/CocktailAPIService.kt
@@ -1,19 +1,26 @@
 package com.example.cocktails.service
 
 import com.example.cocktails.model.Cocktail
+import com.google.gson.GsonBuilder
 import io.reactivex.Single
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class CocktailAPIService {
 
     //https://github.com/AytacAgma/CocktailRecipes/blob/master/app/cocktails.json
 
     private val BASE_URL = "https://github.com/"
+
+    var gson = GsonBuilder()
+        .setLenient()
+        .create()
+
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
         .create(CocktailAPI::class.java)
