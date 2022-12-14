97a34227e6bf65ed1ec7da7bb7d7a03426f74842
bkraytac <aytac.agma@sokmarket.com.tr>
2022-12-14 17:13:53 +0300

    Glide was implemented for imageview with extension methodology

--- app/src/main/java/com/example/cocktails/adapter/CocktailRecyclerAdapter.kt
+++ app/src/main/java/com/example/cocktails/adapter/CocktailRecyclerAdapter.kt
@@ -6,6 +6,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cocktails.R
 import com.example.cocktails.model.Cocktail
+import com.example.cocktails.util.download
+import com.example.cocktails.util.makePlaceholder
 import kotlinx.android.synthetic.main.cocktails_recycler_row.view.*
 
 class CocktailRecyclerAdapter(val cocktailList : ArrayList<Cocktail>) : RecyclerView.Adapter<CocktailRecyclerAdapter.CocktailViewHolder>() {
@@ -22,6 +24,8 @@ class CocktailRecyclerAdapter(val cocktailList : ArrayList<Cocktail>) : Recycler
     override fun onBindViewHolder(holder: CocktailViewHolder, position: Int) {
         holder.itemView.txtNameRow.text = cocktailList.get(position).name
         holder.itemView.txtTypeRow.text = cocktailList.get(position).type
+
+        holder.itemView.imgCocktailRow.download(cocktailList.get(position).image, makePlaceholder(holder.itemView.context))
     }
 
     override fun getItemCount(): Int {
---
+++ app/src/main/java/com/example/cocktails/util/Util.kt
@@ -0,0 +1,23 @@
+package com.example.cocktails.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.cocktails.R
+
+fun ImageView.download(url: String?, placeholder : CircularProgressDrawable){
+
+    val options = RequestOptions().placeholder(placeholder).error(R.mipmap.ic_launcher_round)
+
+    Glide.with(context).setDefaultRequestOptions(options).load(url).into(this)
+}
+
+fun makePlaceholder(context: Context) : CircularProgressDrawable{
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}
